da3fefb96c365925455c49f3e3da6c6f354b2de3
hung.nh <hung.nh@gnt.com.vn>
2021-01-06 11:55:55 +0700

    update

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,9 @@
 export default {
   // Global page headers (https://go.nuxtjs.dev/config-head)
+  server: {
+    port: 80, // default: 3000
+    host: '0.0.0.0' // default: localhost
+  },
   head: {
     title: 'nuxt-test',
     meta: [
@@ -9,11 +13,45 @@ export default {
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ],
+    script: [
+      {
+        src: 'js/jquery-3.3.1.min.js'
+      }, 
+      {
+        src: 'js/bootstrap.min.js'
+      },
+      {
+        src: 'js/jquery.nice-select.min.js'
+      },
+      {
+        src: 'js/jquery-ui.min.js'
+      },
+      {
+        src: 'js/jquery.slicknav.js'
+      },
+      {
+        src: 'js/mixitup.min.js'
+      },
+      {
+        src: 'js/owl.carousel.min.js'
+      },
+      {
+        src: 'js/main.js'
+      }
     ]
   },
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
   css: [
+    '@assets/css/bootstrap.min.css',
+    "@assets/css/font-awesome.min.css",
+    "@assets/css/elegant-icons.css",
+    "@assets/css/nice-select.css",
+    // "@assets/css/jquery-ui.min.css",
+    // "@assets/css/owl.carousel.min.css",
+    "@assets/css/slicknav.min.css",
+    "@assets/css/style.css"
   ],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
